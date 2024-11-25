3ee3e68d516b1bb4eac65857cbd84b961efd28b7
Hasan-M12 <mhasan259@outlook.com>
2024-11-25 12:06:56 +0000

    this commit completes getting api endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,12 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require("supertest");
+const db = require("../db/connection");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
