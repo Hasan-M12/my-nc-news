6a0a21c6786b8654422808c6ba6815d9ea9aaaad
Hasan-M12 <mhasan259@outlook.com>
2024-11-25 14:30:29 +0000

    updated and added error handling functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const {getApi} = require("./controllers/app.controllers")
+const { getApi, getTopics } = require("./controllers/app.controllers");
+const { wrongPathHandler } = require("./error/app.error");
 
+app.use(express.json());
 
-app.get("/api", getApi)
+app.get("/api", getApi);
 
+app.get("/api/topics", getTopics);
 
+app.use(wrongPathHandler);
 
 module.exports = app;
--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,5 +1,16 @@
-const endpointsJson = require("../endpoints.json")
+const endpointsJson = require("../endpoints.json");
+const { selectTopics } = require("../models/app.models");
 
 exports.getApi = (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
+};
+
+exports.getTopics = (req, res, next) => {
+  selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
